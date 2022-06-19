093fc1379d2250fca762e80e58f972a7b80a51ef
Xsj <2427970264@qq.com>
2022-06-19 21:55:36 +0800

    [两数之和]

---
+++ Jvm_Demo/src/main/java/org/example/twoSum/TwoDataAdd.java
@@ -0,0 +1,48 @@
+package org.example.twoSum;
+
+/**
+ * @description: 两数相加
+ * @author: Xsj
+ * @create: 2022-06-19 15:53
+ **/
+public class TwoDataAdd {
+
+
+    /**
+     * 题目说明:给你两个非空 的链表，表示两个非负的整数。它们每位数字都是按照逆序的方式存储的，并且每个节点只能存储一位数字。
+     * 请你将两个数相加，并以相同形式返回一个表示和的链表。
+     * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+     * 示例：
+     *     输入：l1 = [2,4,3], l2 = [5,6,4]
+     *     输出：[7,0,8]
+     *     解释：342 + 465 = 807.
+     *
+     * @param l1
+     * @param l2
+     * @return
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+
+        throw new RuntimeException("两数相加异常");
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+}
---
+++ Jvm_Demo/src/main/java/org/example/twoSum/TwoSun.java
@@ -0,0 +1,44 @@
+package org.example.twoSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @description: 两数之和
+ * @author: Xsj
+ * @create: 2022-06-19 08:37
+ **/
+public class TwoSun {
+
+    /**
+     * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
+     * 示例：
+     *      输入：nums = [2,7,11,15], target = 9
+     *      输出：[0,1]
+     *      解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+     * 解题收获：
+     *      1、对于多次出现的变量，用一个临时变量来进行接受以避免多次取值造成寻址操作浪费时间
+     *      2、map中判断是否存在key用包含关系做判断
+     *      3、对于只需要在结尾返回的数据不需要在开始就进行初始化用变量承接，避免浪费栈空间
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] nums, int target) {
+        // 结果集
+        Map<Integer,Integer> map = new HashMap<>(nums.length - 1);
+        for (int i = 0; i < nums.length; i++) {
+            int tmp = target - nums[i];
+            if (map.containsKey(tmp)){
+                return new int[]{i,map.get(tmp)};
+            }
+            if (map.get(nums[i]) == null){
+                map.put(nums[i],i);
+            }
+        }
+        throw new RuntimeException("无结果");
+    }
+
+
+}
